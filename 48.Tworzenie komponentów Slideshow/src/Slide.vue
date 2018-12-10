7629fdbad4958e31d78fbabbea40ae28905d9c14
Unknown <korneliamoskalewicz@onet.pl>
2018-12-10 23:21:02 +0100

    SlideShow

---
+++ 48.Tworzenie komponentów Slideshow/src/Slide.vue
@@ -0,0 +1,24 @@
+<template>
+    <figure class="figure">
+        <img :src="url" alt="" class="img-responsive">
+        <figcaption class="text-centre text-gray nt-2">{{number}}</figcaption>
+    </figure>
+</template>
+<script>
+    export default{
+        name:"Slide",
+        props:{
+            url:String,
+            number:String
+        }
+    };
+</script>
+
+<style scoped>
+    figure.figure{
+        transition:opacity 0.2s ease-in;
+    }
+    .dinmed{
+        opacity: 0.5;
+    }
+</style>
